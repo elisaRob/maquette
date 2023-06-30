022901c832dc19de735baa4c73fbb6c0480ab905
FrckBou <franckb84@live.fr>
2023-06-30 15:36:18 +0200

    maj placement titre form

--- form.php
+++ form.php
@@ -10,42 +10,44 @@
         Nous sommes toujours ouvert à la discussion ou autres remarques.
         N'oublie pas tu peux nous suivre sur les réseaux.</p>
     </div>
-    <form action="reponse.php" method="POST">
-    <h2>Formulaire de contact</h2> 
-        <div>
-            <!-- <label for="nom" class="titreInput">Nom :</label> -->
-            <input type="text"  name="nom" required placeholder= "Vos Noms et Prénoms">
-        </div>
-        <!-- <div>
-            <label for="prenom"class="titreInput">Prénom :</label>
-            <input type="text" name="prenom"required>
-        </div> -->
-        <div>
-            <!-- <label for="mail"class="titreInput">e-mail :</label> -->
-            <input type="email" name="mail" required placeholder= "Mail">
-        </div>
-        <div>
-            <!-- <label for="sujet"class="titreInput">Sujet :</label> -->
-            <input type="text" name="sujet" placeholder= "Le sujet ">
-        </div>
-        <div>
-            <!-- <label for="msg"class="titreInput">Message :</label> -->
-            <textarea id="msg" name="message" rows="10" cols="30" placeholder= "Votre message ici"required></textarea>
-        </div>
-        <div>
+    <!-- <div class="secForm"> -->
+        
+        <form action="reponse.php" method="POST">
+            <div>
+                <!-- <label for="nom" class="titreInput">Nom :</label> -->
+                <input type="text"  name="nom" required placeholder= "Vos Noms et Prénoms">
+            </div>
+            <!-- <div>
+                <label for="prenom"class="titreInput">Prénom :</label>
+                <input type="text" name="prenom"required>
+            </div> -->
+            <div>
+                <!-- <label for="mail"class="titreInput">e-mail :</label> -->
+                <input type="email" name="mail" required placeholder= "Mail">
+            </div>
+            <div>
+                <!-- <label for="sujet"class="titreInput">Sujet :</label> -->
+                <input type="text" name="sujet" placeholder= "Le sujet ">
+            </div>
+            <div>
+                <!-- <label for="msg"class="titreInput">Message :</label> -->
+                <textarea id="msg" name="message" rows="10" cols="30" placeholder= "Votre message ici"required></textarea>
+            </div>
+            <div>
             
-            <input type="checkbox" name="check" id="">
-            <label for="check">Veuillez valider les conditions générales</label>
-        </div>
-        <div>
-            
-            <input type="checkbox" name="check" id="">
-            <label for="check">En validant ce formulaire, vous acceptez d'être contacté</label>
-        </div>
-        <div class="soumettre">
-            <button class="btnForm" type="submit">Soumettre</button>
-        </div>
-    </form>
+                <input type="checkbox" name="check" id="">
+                <label for="check">Veuillez valider les conditions générales</label>
+            </div>
+            <div>
+                <input type="checkbox" name="check" id="">
+                <label for="check">En validant ce formulaire, vous acceptez d'être contacté</label>
+            </div>
+            <div class="soumettre">
+                <button class="btnForm" type="submit">Soumettre</button>
+            </div>
+        </form>
+        <h2 class="formTitle">Formulaire de contact <br> <br> <br> </h2>
+    <!-- </div> -->
 </section>
 <?php
     require_once 'footer.php';
