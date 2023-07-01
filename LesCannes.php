001ee5ea6beea48182e63ea6bedb26edcc84ca0b
Elisabeth ROBERT AOUCHER <elisabeth.robert.aoucher@gmail.com>
2023-07-01 22:52:03 +0200

    page de Valentin

--- LesCannes.php
+++ LesCannes.php
@@ -6,11 +6,118 @@
     require_once 'header.php';
 ?>
 
+<div class="containerHeaderImageLaPecheALaCarpe1">
+    <img src="assets/img/visu_int_carpe.png" alt="bannière">
+    <h1>La pêche à la carpe</h1>
+    <p>Accueil > <span>Pêche à la carpe</span> </p>
+</div>
+
 <!--------------------------------CANNES FOX EOS X3 HORIZON X5 HORIZON------------------------------------------------------------------------------------------------>
   
+<div class="conainerMainPrincipalLesCannes">
+
+  <h2 class="fox">Cannes</h2>
+  <img class="imageFox" src="assets/img/logo_fox.png" alt="FOX">
+
+  <div class="divImagesEtTexte image1">
+      <img src="assets/img/visu_eos.png" alt="canne image">
+      <div class="texte texte1">
+        <div class="interieurTexte interieurTexte1">
+          <h2>Eos</h2>
+          <p> 10’ 12’ 13’ </p>
+          <p>Ces cannessont parfaites les pêcheurs qui s’initient 
+            à la pratique de la
+             pêche à la carpe. 
+             Budget serré mais produit de qualité.</p>
+        </div>
+      </div>
+  </div>
+
+  <div class="divImagesEtTexte image2">
+      <img src="assets/img/visu_horizon_x3.png" alt="canne a pache">
+      <div class="texte texte2">
+        <div class="interieurTexte interieurTexte2">
+          <h2>horizon x3</h2>
+          <p> 10’ 12’ 13’ </p>
+          <p>Rapport qualité/prix incroyable et idéale
+             pour les pêcheurs désireux de limiter leur budget sans renoncer à la qualité Fox !</p>
+        </div>
+      </div>
+  </div>
+
+  <div class="divImagesEtTexte image3">
+      <img src="assets/img/visu_horizon_x5.png" alt="canne a peche">
+      <div class="texte texte3">
+        <div class="interieurTexte interieurTexte3">
+          <h2>horizon x5</h2>
+          <p> 10’ 12’ 13’ </p>
+          <p>Les X5-S utilisent du carbone à la pointe de la technologie pour obtenir des blank plus légers, 
+            plus fins et plus puissants à la fois.</p>
+        </div>
+      </div>
+  </div>
+
+  <h2 class="caperlan">Cannes</h2>
+  <img class="imageCaperlan" src="assets/img/logo_caperlan.png" alt="Caperlan">
+
+  <div class="divImagesEtTexte image4">
+      <img src="assets/img/visu_xtrem500.png" alt="canne a peche">
+      <div class="texte texte4">
+        <div class="interieurTexte interieurTexte4">
+          <h2>xtrem500 </h2>
+     
+          <p>La canne carpe Xtrem 500 représente un excellent compromis entre sensibilité,
+             résistance, puissance et accessibilité.</p>
+        </div>
+      </div>
+  </div>
+
+  <div class="divImagesEtTexte image5">
+      <img src="assets/img/visu_xtrem500.png" alt="canne a la peche">
+      <div class="texte texte5">
+        <div class="interieurTexte interieurTexte5">
+          <h2>xtrem9 </h2>
+        
+          <p>La puissance du blank couplé à l’action de pointe favorise la propulsion du montage à longue distance.
+             C’est une canne taillée pour la longue distance.</p>
+        </div>
+      </div>
+  </div>
+
+  <div class="divImagesEtTexte image6">
+      <img src="assets/img/visu_xtrem500.png" alt="canne a la peche">
+      <div class="texte texte6">
+        <div class="interieurTexte interieurTexte6">
+          <h2>xtrem900 </h2>
+         
+          <p>4 gammes de cannes Xtrem 900 : Power, Full cork, Suprem et Compakt. 
+            Chaque gamme correspond à un usage spécifique.</p>
+        </div>
+      </div>
+  </div>
+
+  <h2 class="korda">Cannes</h2>
+  <img class="kordaImages" src="assets/img/logo_korda.png" alt="pub korda">
+
+  <div class="divImagesEtTexte image7">
+      <img src="assets/img/visu_Kaizen_x6 .png" alt="">
+      <div class="texte texte7">
+        <div class="interieurTexte interieurTexte7">
+          <h2>Kaizen x6</h2>
+       
+          <p>Selon les spécialistes, les accessoires Korda sont les meilleurs du marché et surtout les plus
+             recherchés des Carpistes chevronnés partout en Europe !</p>
+        </div>
+      </div>
+  </div>
 
 
-<section class="image-section">
+
+
+</div>
+
+
+<!-- <section class="image-section">
     <h2 class="section-title">Cannes</h2>
 
 
@@ -54,8 +161,8 @@
       Ces cannes sont parfaites pour les pêcheurs qui s'initient à la pratique<br> 
       de la pêche à la carpe.Budget serré mais produit d'une qualité </p>
       </div>
-  </section>
+  </section> -->
 
 <?php
-require_once 'footer.php';
+// require_once 'footer.php';
 ?>
